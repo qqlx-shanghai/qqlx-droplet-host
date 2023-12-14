e6f164441942794659980cc5339fdcee18d201d4
huangwenqiang <huangwenqiang@shoptop.com>
2023-12-14 20:09:39 +0800

    updates

--- src/node/controller.tcp.ts
+++ src/node/controller.tcp.ts
@@ -6,23 +6,39 @@ import { EventPattern, MessagePattern } from "@nestjs/microservices";
 
 import type { PondDroplet, getPondDropletDto, getPondDropletRes, patchPondDropletDto, patchPondDropletRes } from "qqlx-core";
 import { PATH_POND_DROPLET, SHANGHAI_POSTGRESQL_DROPLET } from "qqlx-core";
-import { toNumber, toString, ToResponse } from "qqlx-cdk";
+import { toNumber, toString, ToResponse, isValid } from "qqlx-cdk";
 import { } from "qqlx-sdk";
 
 @Controller()
 export default class {
-    service_cache = new Map<string, PondDroplet>()
+    private _cache = new Map<string, PondDroplet>()
 
     constructor() {
         const config_file = fs.readFileSync(path.join(process.cwd(), "../qqlx-config.json"), "utf-8")
         const config = JSON.parse(config_file)
 
-        this.service_cache.set(SHANGHAI_POSTGRESQL_DROPLET, {
+        // 服务配置
+        this._cache.set(SHANGHAI_POSTGRESQL_DROPLET, {
             name: config.db_name,
             lan_ip: config.db_host,
             port: config.db_port,
             text: `${config.db_username};${config.db_passwd}`
         })
+
+        // 其他配置
+        for (const key in config) {
+            if (/db_/.test(key)) continue
+
+            const value = toString((config)[key])
+            if (isValid(key) && isValid(value)) {
+                this._cache.set(key, {
+                    name: key,
+                    lan_ip: "",
+                    port: -1,
+                    text: value
+                })
+            }
+        }
     }
 
     @MessagePattern(`${PATH_POND_DROPLET}/get`)
@@ -32,7 +48,7 @@ export default class {
 
         return {
             name: name,
-            droplet: this.service_cache.get(name)
+            droplet: this._cache.get(name)
         }
     }
 
@@ -48,7 +64,7 @@ export default class {
                 port: toNumber(dto.droplet.port),
                 text: toString(dto.droplet.text)
             }
-            this.service_cache.set(name, droplet)
+            this._cache.set(name, droplet)
         }
 
         return null
