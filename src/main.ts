30e58056d04bac97cd79f256f7a7da98f3e5730a
huangwenqiang <huangwenqiang@shoptop.com>
2023-12-19 18:29:30 +0800

    updates

--- src/main.ts
+++ src/main.ts
@@ -6,24 +6,21 @@ import { } from "qqlx-cdk";
 import { getLocalNetworkIPs } from "qqlx-sdk";
 
 import { TcpModule } from "./tcp/module";
+import { PORT } from "./tcp/_"
 
 async function bootstrap () {
-    const TCP_PORT = 1001;
-    const HTTP_PORT = 2001;
 
     // 对内的微服务
     const microservice = await NestFactory.createMicroservice<MicroserviceOptions>(TcpModule, {
         transport: Transport.TCP,
-        options: { host: "0.0.0.0", port: TCP_PORT },
+        options: { host: "0.0.0.0", port: PORT },
     });
     await microservice.listen();
 
     // System tips
-    console.log("\n---- ---- ---- main.ts / qqlx-droplet-host");
+    console.log("\n---- ---- ---- main.ts @qqlx-droplet-host");
     const ips = getLocalNetworkIPs();
     for (const ip of ips) console.log(`${Object.values(ip).reverse().join(".")}`);
-    console.log(`tcp: ${TCP_PORT}`);
-    console.log(`http: ${HTTP_PORT}`);
-    console.log("---- ---- ---- success!");
+    console.log(`---- ---- ---- success on @tcp:${PORT}`);
 }
 bootstrap();
